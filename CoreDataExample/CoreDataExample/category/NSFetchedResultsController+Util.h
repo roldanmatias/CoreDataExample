d10fdf3644a71711b2d7843ca7f54ee493a5d8fd
Matias <roldanmatias@gmail.com>
2012-02-01 13:56:01 -0300

    CoreData

---
+++ CoreDataExample/CoreDataExample/category/NSFetchedResultsController+Util.h
@@ -0,0 +1,13 @@
+//
+//  NSFetchedResultsController+Util.h
+//  CoreDataExample
+//
+//  Created by Matias Roldan on 2/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+@interface NSFetchedResultsController (Util)
+
+@end
---
+++ CoreDataExample/CoreDataExample/category/NSFetchedResultsController+Util.m
@@ -0,0 +1,13 @@
+//
+//  NSFetchedResultsController+Util.m
+//  CoreDataExample
+//
+//  Created by Matias Roldan on 2/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "NSFetchedResultsController+Util.h"
+
+@implementation NSFetchedResultsController (Util)
+
+@end
